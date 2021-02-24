3e0787606099b43d9aedaa4dfc6a9f736c5fe21d
rajan_03@hotmail.com <rajanshrestha80@gmail.com>
2021-02-24 16:52:54 -0600

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import Greeter from './components/Greeter';
-
-
+import Greeter from "./components/Greeter";
 
 const App = () => {
-    const [username,setusername] =useState('');
-    const handleinputonClick=()=>{
-        setusername(username );
     
 
-        
-    }
-  return (
+  const [username, setusername] = useState("");
+  const [loaded, setloaded] = useState(false);
+  useEffect( ()=>{
+    setTimeout( ()=>{setloaded(true) }, 3000);
+ }, []);
+  
+  
+  const handleinputonChange = (e) => {
+    setusername(e.target.value);
+  };
+
+  const handleButtonClick = (e) => {
+    setloaded(true);
+  };
+  
+  if (loaded) {
+    return (
       <div>
-          <Greeter phrase="Hello there," name=" Luke!"/>
-          <Greeter phrase="Hello there," name=" Jake"/>
-          <Greeter phrase="Hello there," name=" Josh"/>
+        <Greeter phrase="Hello there," name=" Luke!" />
+        <Greeter phrase="Hello there," name=" Jake" />
+        <Greeter phrase="Hello there," name=" Josh" />
 
-<input onClick={handleinputonClick} ></input>
-<p>'You are logging in as: {username} '</p>
-<button onClick>Website Loading</button>
-
+        <input onChange={(e) => handleinputonChange(e)}></input>
+        <p>'You are logging in as: {username} '</p>
       </div>
-  )
-};
+    );
+  } else {
+      return(
+    <>
+      <h1>loading</h1>
+      <button onClick={(e) => handleButtonClick(e)}>click Me</button>
+    </>);
+  }
+
+}
 export default App;
